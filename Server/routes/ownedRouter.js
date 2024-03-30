cb161858fddf867659a8298be35f4fab15b71a65
David Ogden <david-ogden699@hotmail.com>
2024-03-29 22:38:46 -0500

    Middleware added to parse json request bodys. POST route to add ingredients is functional

--- Server/routes/ownedRouter.js
+++ Server/routes/ownedRouter.js
@@ -11,7 +11,7 @@ ownedRouter
     })
     .get((req, res, next) => {
         pool.query(
-            'SELECT ingredient_id FROM ingredients_owned WHERE user_id = $1',
+            'SELECT ingredient_id FROM ingredients_owned WHERE user_id = $1;',
             [req.params.userId]
         )
             .then((result) => {
@@ -20,6 +20,51 @@ ownedRouter
                 } else {
                     res.statusCode = 404;
                     err_msg = `User ${req.params.userId} owns no ingredients.`;
+                    return res.json(err_msg);
+                }
+            })
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        pool.query(
+            'SELECT * FROM ingredients_owned WHERE user_id = $1 AND ingredient_id = $2;',
+            [req.params.userId, req.body.ingredientId]
+        )
+            .then((result) => {
+                if (!result.rowCount) {
+                    pool.query(
+                        'INSERT INTO ingredients_owned (user_id, ingredient_id) VALUES ($1,$2);',
+                        [req.params.userId, req.body.ingredientId]
+                    ).then(() =>
+                        res.end(
+                            `Added ingredient ${req.body.ingredientId} to list`
+                        )
+                    );
+                } else {
+                    res.end('Item already in list');
+                }
+            })
+            .catch((err) => next(err));
+    });
+
+ownedRouter
+    .route('/:userId/:ingredientId')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        pool.query(
+            'SELECT ingredient_id FROM ingredients_owned WHERE user_id = $1 AND ingredient_id = $2',
+            [req.params.userId, req.params.ingredientId]
+        )
+            .then((result) => {
+                if (result.rowCount) {
+                    res.json(result.rows);
+                } else {
+                    res.statusCode = 404;
+                    err_msg = `User ${req.params.userId} does not have ingredient ${req.params.ingredientId}.`;
                     return res.json(err_msg);
                 }
             })

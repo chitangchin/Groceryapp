f20735aff0eb5a1d7dbc01b70a7f7c0593ce0a44
parvinder <parvinderbabar@gmail.com>
2024-03-30 06:18:59 -0400

    adding recipe details

--- Client/src/app/Recipes/page.jsx
+++ Client/src/app/Recipes/page.jsx
@@ -12,15 +12,29 @@ const Recipes = () => {
 
   const fetchRecipes = async () => {
     try {
-      const apiKey = 'ae9fab0183fd48e9b6af4a983da4897f';
-      const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${apiKey}`); // Fetching 10 random recipes
+      const apiKey = 'ae9fab0183fd48e9b6af4a983da4897f'; 
+      const response = await fetch(`https://api.spoonacular.com/recipes/informationBulk?apiKey=${apiKey}`);
       if (!response.ok) {
         throw new Error('Failed to fetch recipes');
       }
       const data = await response.json();
-      setRecipes(data.recipes); // Set the array of recipes
+      setRecipes(data);
     } catch (error) {
       console.error('Error fetching recipes:', error);
+    }
+  };
+
+  const viewRecipeDetails = async (recipeId) => {
+    try {
+      const apiKey = 'ae9fab0183fd48e9b6af4a983da4897f'; 
+      const response = await fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch recipe details');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching recipe details:', error);
     }
   };
 
@@ -28,14 +42,14 @@ const Recipes = () => {
     <div>
       <h1>Recipes to Select</h1>
       <div className="recipes-list">
-              {recipes.map(recipe => (
-            
+        {recipes.map(recipe => (
           <div key={recipe.id} className="recipe-item">
-            <Link href={`/recipes/${recipe.id}`}>
-                          <p>{recipe.id}</p>
+            <Link href={`/recipes/${recipe.id}`} passHref>
+              <div onClick={() => viewRecipeDetails(recipe.id)}>
                 <Image src={recipe.image} width={300} height={300} alt={recipe.title} />
+                <p>{recipe.id}</p>
                 <p>{recipe.title}</p>
-             
+              </div>
             </Link>
           </div>
         ))}

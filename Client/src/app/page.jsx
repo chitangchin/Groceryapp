f8c31ced0d011240e5be3f68777e8631598fc754
Brandon <chitangb.chin@gmail.com>
2024-04-03 22:23:05 -0400

    testing

--- Client/src/app/page.jsx
+++ Client/src/app/page.jsx
@@ -3,9 +3,6 @@
 import Link from "next/link";
 export default function Home() {
 
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
 
   return (
    

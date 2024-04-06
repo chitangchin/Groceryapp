7b7f520df57e53385973909b9c93046056e95325
AlehKhvasko <57001092+AlehKhvasko@users.noreply.github.com>
2024-04-06 09:28:46 -0500

    init set up for hashing

---
+++ Server/utils/passwordHashing.js
@@ -0,0 +1,6 @@
+export const hashPassword = (password) => {
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
+}

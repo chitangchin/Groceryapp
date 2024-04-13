e7adb015b2c264094d62c31b7196b48013dc95e1
AlehKhvasko <57001092+AlehKhvasko@users.noreply.github.com>
2024-04-13 16:09:59 -0500

    spell correction

--- Server/routes/spoonacular.route.js
+++ Server/routes/spoonacular.route.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const logger = require('pino')()
 
 router
-    .route('/spoonacular/recipies')
+    .route('/spoonacular/recipes')
     .post(async (req, res) => {
         try{
             const { ingredients } = req.body;
@@ -14,12 +14,12 @@ router
             }
             const ingredientsForApi = ingredients.join(",+");
             const apiKey = process.env.SPOONACULAR_API_KEY;
-            const requestRecepies = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsForApi}&apiKey=${apiKey}`;
-            const requestRecepiesResponse = await fetch(requestRecepies);
-            if(requestRecepiesResponse.ok){
-                const recepies = await requestRecepiesResponse.json();
-                logger.info("recepies: " + recepies);
-                res.status(200).json(recepies);
+            const requestRecipes = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientsForApi}&apiKey=${apiKey}`;
+            const requestRecipesResponse = await fetch(requestRecipes);
+            if(requestRecipesResponse.ok){
+                const recipes = await requestRecipesResponse.json();
+                logger.info("recepies: " + recipes);
+                res.status(200).json(recipes);
             } else {
                 console.error(FAILED_TO_RETRIEVE_RECIPES);
                 throw new Error(FAILED_TO_RETRIEVE_RECIPES);

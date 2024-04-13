686711ee1386606bdafac8873edafeaba1ad1673
AlehKhvasko <57001092+AlehKhvasko@users.noreply.github.com>
2024-04-13 16:12:09 -0500

    import changes

--- Server/routes/spoonacular.route.js
+++ Server/routes/spoonacular.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { BAD_REQUEST, INTERNAL_SERVER_ERROR } = require('../constants');
+const { BAD_REQUEST, INTERNAL_SERVER_ERROR, FAILED_TO_RETRIEVE_RECIPES } = require('../constants');
 const router = express.Router();
 require('dotenv').config();
 const logger = require('pino')()

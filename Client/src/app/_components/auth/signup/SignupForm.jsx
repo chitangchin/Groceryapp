a7d33a5974021358c81f20f1900757f42da9437d
AlehKhvasko <57001092+AlehKhvasko@users.noreply.github.com>
2024-04-15 23:35:27 -0500

    icons, validation

--- Client/src/app/_components/auth/signup/SignupForm.jsx
+++ Client/src/app/_components/auth/signup/SignupForm.jsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from 'react'
-
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
 export const SignupForm = () => {
   const router = useRouter();
 
@@ -10,6 +11,7 @@ export const SignupForm = () => {
   const [repeatPassword, setRepeatPassword] = useState('');
   const [acceptTerms, setAcceptTerms] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
+  const [error, setError] = useState(false);
 
   function handleSignUp() {
       console.log('Signing up with:', username, email, password, repeatPassword);
@@ -20,7 +22,6 @@ export const SignupForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Simulate API call for authentication (replace with your actual logic)
     try {
       const response = await fetch('/api/login', {
         method: 'POST',
@@ -35,7 +36,6 @@ export const SignupForm = () => {
       const data = await response.json();
       console.log('Login successful:', data);
 
-      // Redirect to a different page after successful login (optional)
       router.push('/dashboard');
     } catch (error) {
       setErrorMsg('Invalid email or password');
@@ -45,61 +45,68 @@ export const SignupForm = () => {
   return (
     <div>
       <div className="grid grid-cols-10 grid-rows-10 gap-4 h-screen bg-gray-800 bg-opacity-50">
-                <div className="col-start-2 col-span-3 row-start-3 row-span-6  border border-gray-300 bg-white rounded-lg p-8 opacity-85">
-                    <div className="mb-4 flex items-center">
-                        <input
-                            type="text"
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                        />
+                <div className="col-start-2 col-span-3 row-start-3 row-span-5 text-xs shadow-black shadow-2xl bg-white rounded-lg p-8 opacity-85">
+                    <h2 className="text-2xl text-black text-center font-bold ">Sign Up</h2>
+                    <div className=" space-y-6 flex-col justify-center items-center mt-4">
+                    <div className="relative">
+                      <FaUser className="absolute inset-2 left-2 
+                        flex items-center  
+                        text-base text-gray-500 " /> 
+                      <input
+                          id="username"
+                          type="text"
+                          placeholder="Enter username"
+                          value={username}
+                          required
+                          onChange={(e) => setUsername(e.target.value)}
+                          className="text-black w-full px-4 py-2 pl-8 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                      />
                     </div>
-                    <div className="mb-4 flex items-center">
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                        />
+                    <div className="relative">
+                      <RiLockPasswordFill className="absolute inset-2 left-2
+                      flex items-center  
+                      text-base text-gray-500 " />
+                      <input
+                          type="password"
+                          placeholder="Password"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          className="text-black w-full px-4 py-2 pl-8 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                      />
                     </div>
-                    <div className="mb-4 flex items-center">
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                    </div>
-                    <div className="mb-4 flex items-center">
-                        <input
-                            type="password"
-                            placeholder="Repeat Password"
-                            value={repeatPassword}
-                            onChange={(e) => setRepeatPassword(e.target.value)}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-                        />
-                    </div>
-                    <div className="mb-4 flex items-center">
+                    <div className="relative">
+                      <RiLockPasswordFill className="absolute inset-2 left-2
+                      flex items-center  
+                      text-base text-gray-500 " />
+                      <input
+                          type="password"
+                          placeholder="Repeat Password"
+                          value={repeatPassword}
+                          required
+                          onChange={(e) => setRepeatPassword(e.target.value)}
+                          className="text-black w-full px-4 py-2 pl-8 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                      />
+                    </div>     
+                        
+                        <div className="flex mx-3">
                         <input
                             type="checkbox"
                             checked={acceptTerms}
                             onChange={(e) => setAcceptTerms(e.target.checked)}
                             className="mr-2"
                         />
-                        <label className="text-gray-500 text-sm">
+                        <label className="text-gray-500 m-auto">
                             I accept Terms and Conditions
                         </label>
-                    </div>
+                        </div>
                     <button
                         onClick={handleSignUp}
-                        className="w-full px-4 py-2 text-white bg-teal-500 rounded-md hover:bg-teal-600 focus:outline-none focus:bg-teal-600"
-                        disabled={!acceptTerms} // Disable button if terms are not accepted
+                        className="w-full px-4 py-2 text-white bg-[#f55a3e] rounded-md disabled:bg-gray-500 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-orange-800 focus:outline-none focus:bg-orange-800"
+                        disabled={!acceptTerms || error} 
                     >
                         Sign Up
                     </button>
+                    </div>
                     
                 </div>
             </div>

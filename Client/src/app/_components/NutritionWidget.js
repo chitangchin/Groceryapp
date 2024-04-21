160f049cc6c239a32f5925cffee83fa2025ec53f
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2024-04-21 17:18:07 -0500

    Edited Ingredient page. Couldn't change title

--- Client/src/app/_components/NutritionWidget.js
+++ Client/src/app/_components/NutritionWidget.js
@@ -4,36 +4,13 @@ import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 
 const NutritionWidget = ({ recipeId }) => {
-  const [nutritionData, setNutritionData] = useState(null);
   const [nutritionImage, setNutritionImage] = useState(null);
   //  const apiKey = "ae9fab0183fd48e9b6af4a983da4897f";
   const apiKey = "4ea43c3d4d094e73bd1279ff484b7acf";
     // const apiKey = "ae9fab0183fd48e9b6af4a983da4897f";
 
   useEffect(() => {
-    const fetchNutritionInfo = async () => {
-      try {
-        const response = await fetch(`https://api.spoonacular.com/recipes/${recipeId}/nutritionWidget.json?apiKey=${apiKey}`);
-        if (response.ok) {
-          const data = await response.json();
-          if (data && data.nutrients) {
-            setNutritionData(data.nutrients);
-
-            console.log("Data", data);
-            console.log(Array.isArray(data));
-            console.log("Nutrition", data.nutrients);
-            console.log(Array.isArray(data.nutrients));
-            
-          } else {
-            console.error('Nutritional information not available');
-          }
-        } else {
-          console.error('Failed to fetch nutrition info');
-        }
-      } catch (error) {
-        console.error('Error fetching nutrition info:', error);
-      }
-    };
+    
 
     const fetchNutritionImage = async () => {
       try {
@@ -45,6 +22,8 @@ const NutritionWidget = ({ recipeId }) => {
             'Content-Type': 'image/png'
           },
         });
+
+        console.log("in nurtionr");
 
         if (!response.ok) {
           throw new Error('Failed to fetch image');
@@ -59,50 +38,21 @@ const NutritionWidget = ({ recipeId }) => {
       }
     };
 
-
-
-    fetchNutritionInfo();
     fetchNutritionImage();
   }, [recipeId, apiKey]);
 
+
   return (
-    <div>
-      <div className="w-full max-w-md mx-auto">
-        <h3 className="text-lg font-semibold text-center mb-4">Nutritional Information</h3>
-        <div className="overflow-x-auto">
-          <table className="border-collapse border border-gray-300 rounded-lg overflow-hidden">
-            <thead className="bg-blue-200">
-              <tr>
-                <th className="border border-gray-300 px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Nutrient</th>
-                <th className="border border-gray-300 px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Amount</th>
-                <th className="border border-gray-300 px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">% Daily Needs</th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-300">
-              {nutritionData && nutritionData.map((nutrient, index) => (
-                <tr key={index}>
-                  <td className="border border-gray-300 px-6 py-4 whitespace-nowrap">{nutrient.name}</td>
-                  <td className="border border-gray-300 px-6 py-4 whitespace-nowrap">{nutrient.amount} {nutrient.unit}</td>
-                  <td className="border border-gray-300 px-6 py-4 whitespace-nowrap">{nutrient.percentOfDailyNeeds}%</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-
+    <div className='py-8'>
       {nutritionImage && (
-        <div>
-          <Image src={nutritionImage} alt="nutrition Image" width={500} height={500}/>
-        </div>
+          <Image src={nutritionImage} alt="nutrition Image" width={400} height={400}/>
       )}
-
     </div>
 
-
-    
-
   );
+
+
+
 };
 
 export default NutritionWidget;
--- Client/src/app/dashboard/recipes/[recipeId]/page.js
+++ Client/src/app/dashboard/recipes/[recipeId]/page.js
@@ -2,21 +2,21 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { recipeDetailsDummy, nutritionDummy } from '../../../dummyData';
+import { recipeDetailsDummy } from '../../../dummyData';
 
 import NutritionWidget from '../../../_components/NutritionWidget';
 // import IngredientsPage from '@/app/Components/IngredientsPage.jsx';
 
+
+
 const RecipeDetailPage = (amountRecipe) => {
   const router = useRouter();
   const [recipeDetails, setRecipeDetails] = useState(null);
-  const [nutritionInfo, setNutritionInfo] = useState(null);
   const [shoppingList, setShoppingList] = useState([]);
 
   useEffect(() => {
           setRecipeDetails(recipeDetailsDummy);
           setShoppingList(recipeDetailsDummy?.extendedIngredients.map(ingredient => ({ name: ingredient.original, isChecked: false })) || []);
-          setNutritionInfo(nutritionDummy);
   }, []);
 
   const handleCheckboxChange = (index) => {
@@ -41,59 +41,76 @@ const RecipeDetailPage = (amountRecipe) => {
     return <div className="h-screen flex justify-center items-center">Loading...</div>;
   }
 
-  return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="max-w-4xl mx-auto px-4 py-8 relative">
-        <h1 className="text-3xl font-bold mb-4">{recipeDetails.title}</h1>
-        <div className="text-gray-700 mb-4" dangerouslySetInnerHTML={{ __html: recipeDetails.summary }}></div>
-        <Image src={recipeDetails.image} alt={recipeDetails.title} width={300} height={300} className="mb-4 rounded-lg" />
-        <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">Preparation Time</h2>
-          <p>{recipeDetails.readyInMinutes} minutes</p>
+  return (  
+      <div className="bg-gray-100 min-h-screen">
+        <NextSeo 
+          title={recipeDetails.title} />
+
+        <div className="max-w-4xl mx-auto px-4 py-8 relative">
+          <h1 className="text-3xl font-bold mb-4">{recipeDetails.title}</h1>
+          <div className="text-gray-700 mb-4" dangerouslySetInnerHTML={{ __html: recipeDetails.summary }}></div>
+
+          <div className='flex justify-center relative items-center'>
+            <Image src={recipeDetails.image} alt={recipeDetails.title} width={500} height={500} className="rounded-lg max-h-full max-w-full" />
+          </div>
+
+          <div className="my-4">
+            <h2 className="text-xl font-semibold mb-2">Preparation Time</h2>
+            <p>{recipeDetails.readyInMinutes} minutes</p>
+          </div>
+
+          <div className='flex'>
+            <div className='p-2'>
+              <h2 className="text-2xl font-bold text-center mb-4">Ingredients</h2>
+              {/* <IngredientsPage ingredients={ingredients} /> */}
+              <ul className="list-disc pl-6">
+                {recipeDetails.extendedIngredients.map((ingredient, index) => (
+                  <li key={index}>
+                    <input
+                      type="checkbox"
+                      checked={shoppingList[index] ? shoppingList[index].isChecked : false}
+                      onChange={() => handleCheckboxChange(index)}
+                    />
+                    <label>{ingredient.original}</label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className='p-2'>
+              { <NutritionWidget recipeId={recipeDetails.id} /> }
+            </div>
+
+          </div>
+
+
+          <div className="mb-8">
+            <h2 className="text-2xl font-semibold mb-2">Instructions</h2>
+            {recipeDetails.analyzedInstructions.length > 0 ? (
+              recipeDetails.analyzedInstructions[0].steps.map((step, index) => (
+                <p key={index}><span className="font-semibold">{step.number}:</span> {step.step}</p>
+              ))
+            ) : (
+              <p>No instructions available</p>
+            )}
+          </div>
+
+
+
+          <div className="flex space-x-4 mb-8">
+            <button onClick={addToShoppingList} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Add to Shopping List
+            </button>
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+              Add to Meal Plan
+            </button>
+          </div>
         </div>
-        <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">Instructions</h2>
-          {recipeDetails.analyzedInstructions.length > 0 ? (
-            recipeDetails.analyzedInstructions[0].steps.map((step, index) => (
-              <p key={index}><span className="font-semibold">{step.number}:</span> {step.step}</p>
-            ))
-          ) : (
-            <p>No instructions available</p>
-          )}
-        </div>
-        <div className="flex space-x-4 mb-8">
-          <button onClick={addToShoppingList} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Add to Shopping List
-          </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Add to Meal Plan
-          </button>
-        </div>
+        
+
+
+
       </div>
-      
-      <div className="bg-white py-8">
-        <h2 className="text-2xl font-bold text-center mb-4">Ingredients</h2>
-        {/* <IngredientsPage ingredients={ingredients} /> */}
-        <ul className="list-disc pl-6">
-          {recipeDetails.extendedIngredients.map((ingredient, index) => (
-            <li key={index}>
-              <input
-                type="checkbox"
-                checked={shoppingList[index] ? shoppingList[index].isChecked : false}
-                onChange={() => handleCheckboxChange(index)}
-              />
-              <label>{ingredient.original}</label>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {nutritionInfo && (
-        <div className="bg-white py-8">
-          <h2 className="text-2xl font-bold text-center mb-4">Nutritional Information</h2>
-          { <NutritionWidget recipeId={recipeDetails.id} /> }
-        </div>
-      )}
-    </div>
   );
 };
 

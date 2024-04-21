a8a100223d4fd29475299a20a661b29a263b0e77
Brandon <chitangb.chin@gmail.com>
2024-04-21 10:54:26 -0400

    fixed token

--- Client/src/app/page.js
+++ Client/src/app/page.js
@@ -3,35 +3,35 @@ import { cookies } from 'next/headers'
 
 export default function Home() {
 
-  let token = "";
-
   const data = {
     "username": "Aleh12",
     "password": "usertest1"
   }
 
-  fetch('http://localhost:8080/user/login', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      username: data.username,
-      password: data.password,
-    }),
-  }).then(res => {
-    let cookie = res.headers.get('set-cookie')
-    var arr = cookie.split(";");
-    token = arr[0].split("=")[1];
-  })
 
   async function markAsSeen() {
     'use server'
+    let token = "";
+    await fetch('http://localhost:8080/user/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: data.username,
+        password: data.password,
+      }),
+    }).then(res => {
+      let cookie = res.headers.get('set-cookie')
+      var arr = cookie.split(";");
+      token = arr[0].split("=")[1];
+    })
+    console.log(token)
     cookies().set('token', token,
       { httpOnly: true,
         secure: true,
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000
        },
     );
